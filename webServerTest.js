5a176f7813e63d6805380452200423832d09be81
shinohara8 <55863035+shinohara8@users.noreply.github.com>
2019-09-27 11:14:46 +0900

    Update webServerTest.js

--- webServerTest.js
+++ webServerTest.js
@@ -1,4 +1,6 @@
-// write text
-const pElement = document.createElement('p');
-pElement.innerText = 'this text is remote resource';
-document.body.appendChild(pElement);
+widow.onload = () => {
+	// write text
+	const pElement = document.createElement('p');
+	pElement.innerText = 'this text is remote resource';
+	document.body.appendChild(pElement);
+};
